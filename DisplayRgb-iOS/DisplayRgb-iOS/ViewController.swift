cfa8998cf022bc3db9590422f3f650dfdddd08b5
muhsinetki2 <68248748+muhsinetki2@users.noreply.github.com>
2020-07-16 17:09:49 +0300

    Add files via upload
    The application has been created

---
+++ DisplayRgb-iOS/DisplayRgb-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DisplayRgb-iOS
+//
+//  Created by Muhsin Etki on 16.07.2020.
+//  Copyright © 2020 Muhsin Etki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
